e0ce9f6bf7b9ba4809befc0bd1c1730139c452c8
JaeheeLee-be <clodusky1@gmail.com>
2026-03-22 02:35:51 +0900

    vod practice

--- blog/blog/admin.py
+++ blog/blog/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from blog.models import Blog
+from blog.models import Blog, Comment
 
+admin.site.register(Comment)
+
+
+class CommentInline(admin.TabularInline): # admin page에서 댓글 목록 생성
+    model = Comment
+    fields = ['content', 'author']
+    extra = 1 # 댓글 목록을 1개로 바꿔줌, 일반적인 댓글창으로 바꾼다고 생각하면 됌
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        CommentInline,
+    ]
--- blog/blog/models.py
+++ blog/blog/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+from utils.models import TimeStampModel
+
 User = get_user_model()
 
-class Blog(models.Model):
+class Blog(TimeStampModel):
     CATEGORY_CHOICES = (
         ('free', '자유'),
         ('travel', '여행'),
@@ -23,8 +25,6 @@ class Blog(models.Model):
     # models.SET_NULL: NULL값을 넣습니다. 유저 삭제시 블로그의 author가 null이 됨
     # makemigrations: 기본값이 안정해져 있으면 선택지를 준다. (1.디폴트값, 2.종료)
 
-    created_at = models.DateTimeField('작성일자', auto_now_add=True)
-    updated_at = models.DateTimeField('수정일자', auto_now=True)
 
     def __str__(self):
         return f'[{self.get_category_display()}] {self.title[:10]}'
@@ -40,3 +40,16 @@ class Blog(models.Model):
 
      # category update
      # Blog.objects.filter(category='').update(category='free')
+
+
+class Comment(TimeStampModel):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    content = models.CharField('본문', max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.blog.title} 댓글'
+
+    class Meta:
+        verbose_name = '댓글'
+        verbose_name_plural = '댓글 목록'
