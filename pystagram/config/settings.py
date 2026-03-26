6757d70a96771b6e6947cc57db0a00bd93103a0d
JaeheeLee-be <clodusky1@gmail.com>
2026-03-27 04:41:06 +0900

    Day9 Todo (9일차 + 7일차 피드백 수정)

--- pystagram/config/settings.py
+++ pystagram/config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # own
     'member',
+    'post',
     # 3rd party
     'django_extensions',
 ]
---
+++ pystagram/post/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from post.models import Post, PostImage
+
+
+class PostImageinline(admin.TabularInline):
+    model = PostImage
+    fields = ['image']
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        PostImageinline,
+    ]
